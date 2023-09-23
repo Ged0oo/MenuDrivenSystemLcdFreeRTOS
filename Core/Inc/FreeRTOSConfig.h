0ad13c79282b3f62bf7f9ed2cff394f0e67def2c
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-09-23 07:25:43 +0300

    Update FreeRTOSConfig.h

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -18,7 +18,6 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * http://www.FreeRTOS.org
  * http://aws.amazon.com/freertos
- *
  * 1 tab == 4 spaces!
  */
 /* USER CODE END Header */
